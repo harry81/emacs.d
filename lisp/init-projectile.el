e62438abae18900a2150158affcf63c43af1fa77
Hyunmin Choi <pointer81@gmail.com>
2016-03-16 21:31:38 +0100

    added hlem for projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,6 +1,9 @@
 (require-package 'projectile)
+(require-package 'grizzl)
 
 (projectile-global-mode)
+(setq projectile-completion-system 'helm)
 (setq projectile-completion-system 'grizzl)
+(helm-projectile-on)
 
 (provide 'init-projectile)
